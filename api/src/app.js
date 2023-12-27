4cb2dbd0b359c9e5c840c4082ab6ad6455487f79
Razvan <razvanmarin777@gmail.com>
2023-12-27 21:58:52 +0200

    added some endpoints

--- api/src/app.js
+++ api/src/app.js
@@ -66,3 +66,83 @@ app.post("/create-task", async (req, res) => {
         res.status(500).send("Internal Server Error");
     }
 }   );
+
+
+app.post("update-task", async (req, res) => {
+    const { title, description, status, allocatedToUserId, createdByUserId } = req.body;
+    console.log(req.body)
+    if (!title || !description || !status || !allocatedToUserId || !createdByUserId) {
+        return res.status(400).send("Bad Request");
+    }
+
+    try {
+        const newTask = await Task.update({
+            title: title,
+            description: description,
+            status: status,
+            allocatedToUserId: allocatedToUserId,
+            createdByUserId: createdByUserId
+        });
+
+        res.status(200).send("Task updated successfully!");
+    } catch (error) {
+        console.log("Error updating task:", error);
+        res.status(500).send("Internal Server Error");
+    }
+}   );
+
+app.post("delete-task", async (req, res) => {
+    const { title, description, status, allocatedToUserId, createdByUserId } = req.body;
+    console.log(req.body)
+    if (!title || !description || !status || !allocatedToUserId || !createdByUserId) {
+        return res.status(400).send("Bad Request");
+    }
+
+    try {
+        const newTask = await Task.delete({
+            title: title,
+            description: description,
+            status: status,
+            allocatedToUserId: allocatedToUserId,
+            createdByUserId: createdByUserId
+        });
+
+        res.status(200).send("Task deleted successfully!");
+    } catch (error) {
+        console.log("Error deleting task:", error);
+        res.status(500).send("Internal Server Error");
+    }
+}   );
+
+app.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await Task.find(); 
+        res.status(200).json(tasks);
+    } catch (error) {
+        console.log("Error retrieving tasks:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.post("/set-role", async (req, res) => {
+    const { userId, role } = req.body;
+    if (!userId || !role) {
+        return res.status(400).send("Bad Request");
+    }
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+
+        user.role = role;
+        await user.save();
+
+        res.status(200).send("Role set successfully!");
+    } catch (error) {
+        console.log("Error setting role:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
